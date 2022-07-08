da79648de52c33777f38019f206d1ccd9551ba63
Glsme <glasses.str.man@gmail.com>
2022-07-08 09:38:07 +0900

    Initial Commit

---
+++ Baedal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Baedal
+//
+//  Created by Seokjune Hong on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print(1)
+        print(2)
+        print(3)
+        
+        
+        print(4)
+        print(5)
+    }
+
+
+}
+
